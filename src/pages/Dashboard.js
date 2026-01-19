cf7bbeca17b0cc089b3a4aec2113c64212cefde1
Sanjay Chourasiya <143497835+sanjay2449@users.noreply.github.com>
2026-01-19 11:25:35 +0530

    Import Loader component in Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import AddFileModal from "../components/AddFileModal";
+import Loader from "../components/Loader";
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
